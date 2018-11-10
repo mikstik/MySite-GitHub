a846cda362d4072fe1036af8eac73fa8c3532165
AlexChernov <mikstik@yandex.ru>
2018-11-10 17:17:44 +0300

    Shop v1.1
    Shop v1.1

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE HTML>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Добро пожаловать в LShop!</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+ <header>
+    <div class="header_logo"><p>LShop</p></div>
+        <nav>
+            <div class="topnav" id="myTopnav">
+                <a href="#">Главная</a>
+                <a href="#">Каталог</a>
+                <a href="#">Контакты</a>
+                <a href="#">Отзывы</a>
+                <a href="#">Корзина</a>
+                <a href="#">Регистрация</a>
+                <a href="#">Вход</a>
+                <a href="#" id="menu" class="icon">&#9776</a>                
+            </div>
+        </nav>       
+ </header>
+    <div class="stock">
+        <p><img src="/img/mainpicture.jpg" class="discount"></p>
+    </div>
+    <div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-10" id="searchtext">
+                <p>Вы можете воспользоваться поиском по нашему сайту: </p>
+            </div>
+            <div class="col-2">
+                <p>Текст 2</p>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <p>Текст 1</p>
+            </div>
+            <div class="col-8">
+                <p>Текст 2</p>
+            </div>
+            <div class="col">
+                <p>Текст 3</p>
+            </div>
+    </div>
+</div>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="/js/script.js"></script>
+</body>
+</html>
